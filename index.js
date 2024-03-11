b3ebd29857027a3755d2b8232a01b3c17bf07cdf
orozmat <dj.orozmat.kd@gmail.com>
2024-03-11 07:36:51 +0300

    Added the lesson 2

---
+++ index.js
@@ -0,0 +1,81 @@
+const first = 5;
+
+ const second = 2;
+ 
+ let $number;
+
+ let _number;
+
+ let number2;
+
+ const total = first / second;
+
+ alert('Total:' + total);
+
+
+ const name = 'SATARBEK';
+
+ const position = 'WEB DEV';
+
+ const stringConcat = name + position;
+
+ alert('Name ' + name + ' , ' + 'position' + position);
+
+
+
+
+
+ alert('/саптын узуну' + name.length);
+
+ alert('I get the "T" latter' + name [2]);
+
+ const stringSlice = name.slice(1, 5);
+ alert('сапты кестим' + stringSlice);
+
+
+
+ alert('сапты чоон тамгадан жазабыз' + name.toUpperCase());
+
+ alert('сапты кичине тамгадан жазабыз' + name.toLowerCase());
+
+
+
+ const string1 = "эЙ, жанара, кандай";
+
+ alert(string1[0].toUpperCase() + string1[1].toLowerCase() + ', ' + string1[4].toUpperCase() + string1.slice(5). toLowerCase());
+
+
+
+
+
+
+
+
+
+ const string = "эМнеГе, аЙгаНыш, аСелди, АЛДАДЫ?"
+
+
+
+
+ alert(string[0].toUpperCase() + string[1].toLowerCase() + string[2] + string[3] + string[4].toLowerCase() + string[5] +', ' + string[8].toUpperCase() +
+ string[9].toLowerCase() + string[10] + string[11] + string[12].toLowerCase() + string[13] + string[14] +'. ' + string[17].toUpperCase() + string[18].toLowerCase() + string[19] + string[20] + string[21] +
+ string[22] + ', ' + string[25].toLowerCase() + string[26].toLowerCase() + string[27].toLowerCase() + string[28].toLowerCase() + string[29].toLowerCase() + string[30].toLowerCase() + string[31]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ const adam = 15 * 2;
+ const adam1  = 2 * 2;
+ const adam2 = 7 * 2;
+ alert(adam + adam1 + adam2 + ', ' + 'рекет намаз окулду');
+
